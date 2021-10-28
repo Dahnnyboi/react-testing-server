855603812114490aa9c27a39211f32ed71f31bb5
Dahnyboi <dahn@qualitytrade.com>
2021-10-28 22:58:47 +0800

    Fix cookies

--- src/routes/apis/auth.apis.ts
+++ src/routes/apis/auth.apis.ts
@@ -27,7 +27,6 @@ export default (app: Router): void => {
       // naming should be the same with the frontend
       res.cookie(AUTH_COOKIE_KEY, token, {
         httpOnly: false,
-        secure: true,
       });
       res.status(200).json({ data: { ...req.user[0] } });
     },
@@ -39,7 +38,6 @@ export default (app: Router): void => {
     (req: Request, res: Response) => {
       res.clearCookie(AUTH_COOKIE_KEY, {
         httpOnly: false,
-        secure: true,
       });
       res.status(200).json({ message: 'Successfully logout' });
     },
